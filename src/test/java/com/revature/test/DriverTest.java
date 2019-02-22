46697d38f31a19d1402d26671d13bb65ebd27536
Joshua Mendez <joshuafmendez@gmail.com>
2019-02-22 14:40:13 -0500

    added test

--- src/test/java/com/revature/test/DriverTest.java
+++ src/test/java/com/revature/test/DriverTest.java
@@ -1,5 +1,8 @@
 package com.revature.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -7,6 +10,7 @@ import org.apache.hadoop.mrunit.mapreduce.MapDriver;
 import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
 import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
 import org.junit.Before;
+import org.junit.Test;
 
 import com.revature.map.gsMapper;
 import com.revature.reduce.gsReducer;
@@ -31,6 +35,27 @@ public class DriverTest {
 		mapReduceDriver = new MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable>();
 		mapReduceDriver.setMapper(mapper);
 		mapReduceDriver.setReducer(reducer);
+	}
+	@Test
+	public void testMapper() {
+
+		mapDriver.withInput(new LongWritable(1), new Text("cat cat dog"));
+		mapDriver.withOutput(new Text("cat"), new IntWritable(1));
+		mapDriver.withOutput(new Text("cat"), new IntWritable(1));
+		mapDriver.withOutput(new Text("dog"), new IntWritable(1));
+
+		mapDriver.runTest();
+	}
+
+	@Test
+	public void testReducer() {
+
+		List<IntWritable> values = new ArrayList<IntWritable>();
+		values.add(new IntWritable(1));
+		values.add(new IntWritable(1));
+		reduceDriver.withInput(new Text("cat"), values);
+		reduceDriver.withOutput(new Text("cat"), new IntWritable(2));
+		reduceDriver.runTest();
 	}
 
 }
